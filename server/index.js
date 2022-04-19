1114127495025bfe78e587822704784767019cbb
CDeeW <89137794+CDeeW@users.noreply.github.com>
2022-04-19 21:15:02 +0100

    backend homepage

--- server/index.js
+++ server/index.js
@@ -1,17 +1,65 @@
 const PORT = 8000;
 const express = require('express');
 const { MongoClient } = require('mongodb');
+// dont really understand the : between these 2 below
+const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
 const uri =
   'mongodb+srv://CDW:newpassword@cluster-dating.xg9ca.mongodb.net/Cluster-Dating?retryWrites=true&w=majority';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.json('hssi');
 });
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
   const client = new MongoClient(uri);
+  const { email, password } = req.body;
+
+  const generatedUserId = uuidv4();
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await client.connect();
+    const database = client.db('app-data');
+    const users = database.collection('users');
+
+    //check if user already exists by using email address
+    const existingUser = await users.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(409)
+        .send('user already exists. Please use a different email');
+    }
+
+    const sanitizedEmail = email.toLowerCase();
+
+    const data = {
+      user_id: generatedUserId,
+      email: sanitizedEmail,
+      hashed_password: hashedPassword,
+    };
+
+    // insert the data object into the mongoDB users collection
+    const insertedUser = await users.insertOne(data);
+
+    const token = jwt.sign(insertedUser, sanitizedEmail, {
+      expiresIn: 60 * 24,
+    });
+
+    res
+      .status(201)
+      .json({ token, userId: generatedUserId, email: sanitizedEmail });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get('/users', async (req, res) => {
